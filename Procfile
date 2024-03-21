a3e6321ead6fa59e6ecac8b652ba8adf85eacc40
sudarshan1x1 <iamsudarshan.as@gmail.com>
2024-03-22 03:28:39 +0530

    Second commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python controller.py
+web: gunicorn controller:app
--- controller.py
+++ controller.py
@@ -52,7 +52,7 @@ def get_image():
     return send_file('linear_reg_algo.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
     
 
 
